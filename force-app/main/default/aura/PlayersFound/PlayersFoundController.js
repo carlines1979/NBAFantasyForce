ca0452a6078ed4ea6ccc58bc32d2846f82baac02
Carlos Bermudez <cabermudezr@gmail.com>
2020-08-31 17:07:55 -0400

    player stats-chart

--- force-app/main/default/aura/PlayersFound/PlayersFoundController.js
+++ force-app/main/default/aura/PlayersFound/PlayersFoundController.js
@@ -8,5 +8,18 @@
             helper.fetchPlayers(component,playerName); 
         }
 		
-	}
+	},
+    
+    getSelectedPlayerId : function (component, event, helper){
+        var selectedPlayerId = event.getParam('selectedRows');
+        component.set("v.noPlayerSelected" , false); 
+        component.set("v.playerSelected", selectedPlayerId[0].ServerId__c);
+        component.set("v.playerSelectedName", selectedPlayerId[0].Name);
+        
+    },
+    
+    StatsPlayer : function (component, event, helper){
+        var serverId = component.get("v.playerSelected");
+        helper.getLastSeasonStats(component, serverId); 
+    },
 })
--- force-app/main/default/aura/PlayersFound/PlayersFoundHelper.js
+++ force-app/main/default/aura/PlayersFound/PlayersFoundHelper.js
@@ -54,5 +54,44 @@
         $A.enqueueAction(action);
         
 		
-	}
+	},
+        
+	getLastSeasonStats : function(component,ServerId) {
+        var action = component.get("c.getLastSeasonStats");
+        action.setParams({ServerId: ServerId});
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            
+            if (state === "SUCCESS") {
+                let val = response.getReturnValue() ;
+                
+                var labelset=[] ;
+                var dataset=[] ;
+                val.forEach(function(key) {
+                    labelset.push(key.label) ; 
+                    dataset.push(key.count) ; 
+                });
+                var playerName = component.get("v.playerSelectedName"); 
+                new Chart(document.getElementById("bar-chart"), {
+                    type: 'bar',
+                    data: {
+                        labels:labelset,
+                        datasets: [{
+                            label: playerName,
+                            backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9", "#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9", "#3e95cd"],
+                            data: dataset
+                        }]
+                    },
+                    options: {
+                        title: {
+                            display: true,
+                            text: 'Last Season Player Stats'
+                        }
+                    }
+                });
+                
+            }
+        });
+        $A.enqueueAction(action); 
+    },
 })
